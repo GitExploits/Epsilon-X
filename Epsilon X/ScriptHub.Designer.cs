5d07e6f06bf5cdc5453e81e68781c9a73fab2d41
Admin <Admin@ASUS>
2022-05-07 18:15:55 +0200

    Agregar archivos de proyecto.

---
+++ Epsilon X/ScriptHub.Designer.cs
@@ -0,0 +1,299 @@
+﻿
+namespace Epsilon_X
+{
+    partial class ScriptHub
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button27 = new System.Windows.Forms.Button();
+            this.button28 = new System.Windows.Forms.Button();
+            this.button29 = new System.Windows.Forms.Button();
+            this.button30 = new System.Windows.Forms.Button();
+            this.button31 = new System.Windows.Forms.Button();
+            this.button36 = new System.Windows.Forms.Button();
+            this.button38 = new System.Windows.Forms.Button();
+            this.button39 = new System.Windows.Forms.Button();
+            this.button40 = new System.Windows.Forms.Button();
+            this.button41 = new System.Windows.Forms.Button();
+            this.button42 = new System.Windows.Forms.Button();
+            this.button44 = new System.Windows.Forms.Button();
+            this.button43 = new System.Windows.Forms.Button();
+            this.button45 = new System.Windows.Forms.Button();
+            this.button46 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(500, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ZOぞ Script";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(132, 12);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(95, 35);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "JailBreakScript";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(500, 12);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(95, 35);
+            this.button16.TabIndex = 15;
+            this.button16.Text = "Septex Admin";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button27
+            // 
+            this.button27.Location = new System.Drawing.Point(615, 378);
+            this.button27.Name = "button27";
+            this.button27.Size = new System.Drawing.Size(95, 35);
+            this.button27.TabIndex = 26;
+            this.button27.Text = "Lifting Simulator";
+            this.button27.UseVisualStyleBackColor = true;
+            // 
+            // button28
+            // 
+            this.button28.Location = new System.Drawing.Point(615, 244);
+            this.button28.Name = "button28";
+            this.button28.Size = new System.Drawing.Size(95, 35);
+            this.button28.TabIndex = 27;
+            this.button28.Text = "QWER";
+            this.button28.UseVisualStyleBackColor = true;
+            // 
+            // button29
+            // 
+            this.button29.Location = new System.Drawing.Point(615, 12);
+            this.button29.Name = "button29";
+            this.button29.Size = new System.Drawing.Size(95, 35);
+            this.button29.TabIndex = 28;
+            this.button29.Text = "Nexus Admin";
+            this.button29.UseVisualStyleBackColor = true;
+            this.button29.Click += new System.EventHandler(this.button29_Click);
+            // 
+            // button30
+            // 
+            this.button30.Location = new System.Drawing.Point(615, 66);
+            this.button30.Name = "button30";
+            this.button30.Size = new System.Drawing.Size(95, 35);
+            this.button30.TabIndex = 29;
+            this.button30.Text = "Robux";
+            this.button30.UseVisualStyleBackColor = true;
+            // 
+            // button31
+            // 
+            this.button31.Location = new System.Drawing.Point(615, 119);
+            this.button31.Name = "button31";
+            this.button31.Size = new System.Drawing.Size(95, 35);
+            this.button31.TabIndex = 30;
+            this.button31.Text = "Chat Spam";
+            this.button31.UseVisualStyleBackColor = true;
+            this.button31.Click += new System.EventHandler(this.button31_Click);
+            // 
+            // button36
+            // 
+            this.button36.Location = new System.Drawing.Point(716, 12);
+            this.button36.Name = "button36";
+            this.button36.Size = new System.Drawing.Size(83, 35);
+            this.button36.TabIndex = 35;
+            this.button36.Text = "Fire";
+            this.button36.UseVisualStyleBackColor = true;
+            this.button36.Click += new System.EventHandler(this.button36_Click);
+            // 
+            // button38
+            // 
+            this.button38.Location = new System.Drawing.Point(615, 182);
+            this.button38.Name = "button38";
+            this.button38.Size = new System.Drawing.Size(95, 35);
+            this.button38.TabIndex = 37;
+            this.button38.Text = "Inifnite Yield";
+            this.button38.UseVisualStyleBackColor = true;
+            // 
+            // button39
+            // 
+            this.button39.Location = new System.Drawing.Point(716, 119);
+            this.button39.Name = "button39";
+            this.button39.Size = new System.Drawing.Size(83, 35);
+            this.button39.TabIndex = 38;
+            this.button39.Text = "button39";
+            this.button39.UseVisualStyleBackColor = true;
+            // 
+            // button40
+            // 
+            this.button40.Location = new System.Drawing.Point(716, 66);
+            this.button40.Name = "button40";
+            this.button40.Size = new System.Drawing.Size(83, 35);
+            this.button40.TabIndex = 39;
+            this.button40.Text = "Gorilla";
+            this.button40.UseVisualStyleBackColor = true;
+            // 
+            // button41
+            // 
+            this.button41.Location = new System.Drawing.Point(716, 244);
+            this.button41.Name = "button41";
+            this.button41.Size = new System.Drawing.Size(83, 35);
+            this.button41.TabIndex = 40;
+            this.button41.Text = "SCAMMED";
+            this.button41.UseVisualStyleBackColor = true;
+            // 
+            // button42
+            // 
+            this.button42.Location = new System.Drawing.Point(716, 182);
+            this.button42.Name = "button42";
+            this.button42.Size = new System.Drawing.Size(83, 35);
+            this.button42.TabIndex = 41;
+            this.button42.Text = "BrookHaven";
+            this.button42.UseVisualStyleBackColor = true;
+            this.button42.Click += new System.EventHandler(this.button42_Click);
+            // 
+            // button44
+            // 
+            this.button44.Location = new System.Drawing.Point(716, 323);
+            this.button44.Name = "button44";
+            this.button44.Size = new System.Drawing.Size(83, 35);
+            this.button44.TabIndex = 43;
+            this.button44.Text = "PL Script V2";
+            this.button44.UseVisualStyleBackColor = true;
+            // 
+            // button43
+            // 
+            this.button43.Location = new System.Drawing.Point(260, 12);
+            this.button43.Name = "button43";
+            this.button43.Size = new System.Drawing.Size(95, 35);
+            this.button43.TabIndex = 44;
+            this.button43.Text = "BreakingPoint";
+            this.button43.UseVisualStyleBackColor = true;
+            // 
+            // button45
+            // 
+            this.button45.Location = new System.Drawing.Point(615, 323);
+            this.button45.Name = "button45";
+            this.button45.Size = new System.Drawing.Size(95, 35);
+            this.button45.TabIndex = 45;
+            this.button45.Text = "Adopt me";
+            this.button45.UseVisualStyleBackColor = true;
+            // 
+            // button46
+            // 
+            this.button46.Location = new System.Drawing.Point(716, 378);
+            this.button46.Name = "button46";
+            this.button46.Size = new System.Drawing.Size(83, 35);
+            this.button46.TabIndex = 46;
+            this.button46.Text = "Doge";
+            this.button46.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(385, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 35);
+            this.button4.TabIndex = 47;
+            this.button4.Text = "PL Script";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Symbol", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(242, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 41);
+            this.label1.TabIndex = 48;
+            this.label1.Text = "ScriptHub BETA";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ScriptHub
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button46);
+            this.Controls.Add(this.button45);
+            this.Controls.Add(this.button43);
+            this.Controls.Add(this.button44);
+            this.Controls.Add(this.button42);
+            this.Controls.Add(this.button41);
+            this.Controls.Add(this.button40);
+            this.Controls.Add(this.button39);
+            this.Controls.Add(this.button38);
+            this.Controls.Add(this.button36);
+            this.Controls.Add(this.button31);
+            this.Controls.Add(this.button30);
+            this.Controls.Add(this.button29);
+            this.Controls.Add(this.button28);
+            this.Controls.Add(this.button27);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button1);
+            this.Name = "ScriptHub";
+            this.Text = "ScriptHub";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button27;
+        private System.Windows.Forms.Button button28;
+        private System.Windows.Forms.Button button29;
+        private System.Windows.Forms.Button button30;
+        private System.Windows.Forms.Button button31;
+        private System.Windows.Forms.Button button36;
+        private System.Windows.Forms.Button button38;
+        private System.Windows.Forms.Button button39;
+        private System.Windows.Forms.Button button40;
+        private System.Windows.Forms.Button button41;
+        private System.Windows.Forms.Button button42;
+        private System.Windows.Forms.Button button44;
+        private System.Windows.Forms.Button button43;
+        private System.Windows.Forms.Button button45;
+        private System.Windows.Forms.Button button46;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Epsilon X/ScriptHub.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeAreDevs_API
+
+namespace Epsilon_X
+{
+    public partial class ScriptHub : Form
+    {
+        ExploitAPI module = new ExploitAPI();
+        public ScriptHub()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebClient wb = new WebClient();
+            string Script = wb.DownloadString("https://pastebin.com/raw/tqb6tpNf");
+            module.SendLuaScript(Script);
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            WebClient wb = new WebClient();
+            string Script = wb.DownloadString("https://pastebin.com/raw/W95p1KuL");
+            module.SendLuaScript(Script);
+        }
+
+        private void button29_Click(object sender, EventArgs e)
+        {
+            WebClient wb = new WebClient();
+            string Script = wb.DownloadString("https://pastebin.com/raw/ud5h4Kne");
+            module.SendLuaScript(Script);
+        }
+
+        private void button36_Click(object sender, EventArgs e)
+        {
+            WebClient wb = new WebClient();
+            string Script = wb.DownloadString("https://pastebin.com/raw/wLMAgmiz");
+            module.SendLuaScript(Script);
+        }
+
+        private void button31_Click(object sender, EventArgs e)
+        {
+            WebClient wb = new WebClient();
+            string Script = wb.DownloadString("");
+            module.SendLuaScript(Script);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            WebClient wb = new WebClient();
+            string Script = wb.DownloadString("https://pastebin.com/raw/D6wnPGx8");
+            module.SendLuaScript(Script);
+        }
+
+        private void button42_Click(object sender, EventArgs e)
+        {
+            WebClient wb = new WebClient();
+            string Script = wb.DownloadString("https://pastebin.com/raw/bNcahK57");
+            module.SendLuaScript(Script);
+        }
+    }
+}
